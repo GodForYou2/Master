054bd447fca997c0947329c37ae3f390dcbf3b42
GodForYou2 <saimyomyat194@gmail.com>
2026-06-14 16:23:28 +0630

    Delete main.py

--- main.py
+++
@@ -1,7 +0,0 @@
-# .so ဖိုင်ထဲမှ RuijieBypass class ကို လှမ်းခေါ်ခြင်း
-from yourgod import RuijieBypass
-
-if __name__ == "__main__":
-    # Class ကို Object ဆောက်ပြီး Menu အား Run ခြင်း
-    app = RuijieBypass()
-    app.run_menu()
